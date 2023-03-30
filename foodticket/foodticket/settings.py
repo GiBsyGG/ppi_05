d1a04df2c74cdde494547576cbc0b3a172919b12
GiBsy <jcval2801@gmail.com>
2023-03-30 17:53:35 -0500

    deploy setting

--- foodticket/foodticket/settings.py
+++ foodticket/foodticket/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = '/home/GiBsyGG/ppi_05/foodticket/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

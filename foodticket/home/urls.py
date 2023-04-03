83330c3a2c2d64e40898b35a8be0b0f7a78a5065
GiBsy <jcval2801@gmail.com>
2023-04-03 14:51:20 -0500

    añadidas funciones Auth
    Añadidas funciones de registro, login y logout con algunas templates básicas

--- foodticket/home/urls.py
+++ foodticket/home/urls.py
@@ -7,4 +7,7 @@ app_name = "home"
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("registrarse/", views.registrarse, name="registrarse"),
+    path("cerrar_sesion/", views.cerrar_sesion, name="cerrar_sesion"),
+    path("login/", views.iniciar_sesion, name="login"),
 ]

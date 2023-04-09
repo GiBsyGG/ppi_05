2a200388c76964d817832b6086e266782b5b249a
GiBsy <jcval2801@gmail.com>
2023-04-08 21:42:03 -0500

    modificación modelo menu
    Se adicionaron 2 dias más para ingrear el menú y la opción de diario

--- foodticket/menus/models.py
+++ foodticket/menus/models.py
@@ -3,19 +3,42 @@ from restaurantes.models import Restaurante
 # Create your models here.
 class Menu(models.Model):
     nombre = models.CharField(max_length=100)
-    descripcion = models.CharField(max_length=200)
+    descripcion = models.TextField(blank=True)
     precio = models.IntegerField()
-    DIA_OPCIONES = (
+    DIA1_OPCIONES = (
         ('Lunes', 'Lunes'),
         ('Martes', 'Martes'),
         ('Miércoles', 'Miércoles'),
         ('Jueves', 'Jueves'),
         ('Viernes', 'Viernes'),
         ('Sábado', 'Sábado'),
-        ('Domingo', 'Domingo')
+        ('Domingo', 'Domingo'),
+        ('Diario', 'Diario'),
     )
-    dia = models.CharField(max_length=10, choices=DIA_OPCIONES)
-
+    DIA2_OPCIONES = (
+        ('Lunes', 'Lunes'),
+        ('Martes', 'Martes'),
+        ('Miércoles', 'Miércoles'),
+        ('Jueves', 'Jueves'),
+        ('Viernes', 'Viernes'),
+        ('Sábado', 'Sábado'),
+        ('Domingo', 'Domingo'),
+        ('Diario', 'Diario'),
+    )
+    DIA3_OPCIONES = (
+        ('Lunes', 'Lunes'),
+        ('Martes', 'Martes'),
+        ('Miércoles', 'Miércoles'),
+        ('Jueves', 'Jueves'),
+        ('Viernes', 'Viernes'),
+        ('Sábado', 'Sábado'),
+        ('Domingo', 'Domingo'),
+        ('Diario', 'Diario'),
+    )
+    dia1 = models.CharField(max_length=10, choices=DIA1_OPCIONES)
+    dia2 = models.CharField(max_length=10, choices=DIA2_OPCIONES, blank=True)
+    dia3 = models.CharField(max_length=10, choices=DIA3_OPCIONES, blank=True)
+    
     id_restaurante = models.ForeignKey(Restaurante, on_delete=models.CASCADE)
 
 

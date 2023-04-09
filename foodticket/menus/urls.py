78164c0daebea4ea475458977491b46cffa48aaf
GiBsy <jcval2801@gmail.com>
2023-04-09 15:44:22 -0500

    add view para crear menú

--- foodticket/menus/urls.py
+++ foodticket/menus/urls.py
@@ -7,4 +7,5 @@ app_name = "menus"
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("crear/", views.crear_menu, name="crear_menu"),
 ]
--- foodticket/menus/views.py
+++ foodticket/menus/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
+from django.db import IntegrityError
 
 
+from .models import Menu
+from  restaurantes.models import RestauranteUsuario
 # Create your views here.
 class IndexView(generic.TemplateView):
     template_name = "menus/index.html"
+
+def crear_menu(request):
+    """Crea un menú para el restaurante del usuario que está con la sesión iniciada"""
+    if request.method == "GET":
+        return render(request, "menus/crear_menu.html")
+    else:
+        try:
+            # Buscamos el restaurante del usuario que está creando el menú
+            restaurante = get_object_or_404(RestauranteUsuario, usuario=request.user)
+            menu = Menu.objects.create(
+                nombre=request.POST["menu_nombre"],
+                descripcion=request.POST["menu_descripcion"],
+                precio=request.POST["menu_precio"],
+                dia1=request.POST["menu_dia1"],
+                dia2=request.POST["menu_dia2"],
+                dia3=request.POST["menu_dia3"],
+                id_restaurante=restaurante
+                )
+            
+            # Guardamos el menú en la base de datos
+            menu.save()
+
+            # Redirigimos al usuario a la página de menus
+            return redirect("menus:index")
+        except IntegrityError:
+            return render(request, "menus/crear_menu.html", {
+                "error": "Menú ya creado"
+            })

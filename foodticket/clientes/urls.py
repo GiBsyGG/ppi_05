efa992b249f2a474f25f844af607ef5d6ae76e56
GiBsy <jcval2801@gmail.com>
2023-04-17 11:54:22 -0500

    Correccion en las URLS de clientes
    Se corrigió la forma de las urls en la app clientes

--- foodticket/clientes/urls.py
+++ foodticket/clientes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-
+app_name = "clientes"
 urlpatterns = [
     path('', views.index, name="index"),
     path('compra/', views.compra, name="compra"),
